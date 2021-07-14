ae36374d9933aadb7989986b23f1255b8f5938d9
tequomo <tequomo@gmail.com>
2021-07-15 01:57:18 +0300

    ~

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
 import { uploadForm } from './image-upload.js';
+import { throttle } from './util.js';
 
 // const isValidNewPicture = (curFile) => {
 //   const formats = ['image/jpeg', 'image/jpeg', 'image/png'];
@@ -18,24 +19,32 @@ import { uploadForm } from './image-upload.js';
 const MIN_HASHTAG_LENGTH = 1;
 const MAX_HASHTAG_LENGTH = 20;
 const HASHTAG_COUNT = 5;
+const CHECK_DELAY = 1000;
 
-const hashtagInput = uploadForm.querySelector('.text__hashtags');
+const hashtagField = uploadForm.querySelector('.text__hashtags');
 
 const validateHashtag = (value) => {
-  const re = /^#[A-Za-zА-Яа-я0-9]{1,19}$/;
+
+  let alertString = '';
+  const re = /^#[A-Za-zА-Яа-я0-9]{1,19}$/gi;
+  const reSpace = /\b#\b/;
   // re.test('#хэштег'); // > true
   const hashtags = value.toLowerCase().split([' ']);
-  if(hashtags.length >= HASHTAG_COUNT) {
-    alertString = '';
+
+  if(hashtags.length > HASHTAG_COUNT) {
+    alertString = 'Вы превысили допустимое количество хештегов\n';
   }
 
-  hashtags.forEach((item) => {
-
+  hashtags.forEach((tag) => {
+    // console.log(re.test(tag));
+    alertString += (re.test(tag)) ? '' : (reSpace.test(tag) ? 'Поставьте пробел между хештегами\n' : 'Вы ввели недопустимый хештег\n');
   });
-  console.log(hashtags);
+
+  hashtagField.setCustomValidity(alertString);
+  hashtagField.reportValidity();
 };
 
 
-hashtagInput.addEventListener('input', () => {
-  validateHashtag(hashtagInput.value);
-});
+hashtagField.addEventListener('input', throttle(() => {
+  validateHashtag(hashtagField.value);
+}, CHECK_DELAY));
--- js/util.js
+++ js/util.js
@@ -36,6 +36,39 @@ const closePopup = (node) => {
   document.body.classList.remove('modal-open');
 };
 
+// Функция взята из интернета и доработана
+// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+function throttle (callback, delayBetweenFrames) {
+  // Используем замыкания, чтобы время "последнего кадра" навсегда приклеилось
+  // к возвращаемой функции с условием, тогда мы его сможем перезаписывать
+  let lastTime = 0;
+
+  return (...rest) => {
+    // Получаем текущую дату в миллисекундах,
+    // чтобы можно было в дальнейшем
+    // вычислять разницу между кадрами
+    const now = new Date();
+
+    // Если время между кадрами больше задержки,
+    // вызываем наш колбэк и перезаписываем lastTime
+    // временем "последнего кадра"
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
 const isEscEvent = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
 
-export { getRandomInteger, getRandomLengthArray, checkMaxLength, openPopup, closePopup, isEscEvent };
+export { getRandomInteger, getRandomLengthArray, checkMaxLength, openPopup, closePopup, isEscEvent, debounce, throttle };
