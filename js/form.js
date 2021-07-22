b120b07d19291a0544085487713c28db3123b214
tequomo <tequomo@gmail.com>
2021-07-23 02:15:27 +0300

    ~~~

--- js/form.js
+++ js/form.js
@@ -23,41 +23,84 @@ const CHECK_DELAY = 500;
 
 const hashtagField = uploadForm.querySelector('.text__hashtags');
 
+// const validateHashtag = (value) => {
+
+//   let alertString = '';
+//   const re = /^#[A-Za-zА-Яа-я0-9]{1,19}$/gi;
+//   const reSpace = /\b#\b/;
+//   // re.test('#хэштег'); // > true
+//   if (/\s\s/.test(value)) {
+//     value = value.replace( /  +/g, ' ' );
+//   }
+//   const hashtags = value.trim().toLowerCase().split([' ']);
+//   console.log(`value=${  value}`);
+//   if (!value || value === '') {
+//     alertString = '';
+//   }
+//   console.log(`hashtags=${  hashtags}`);
+
+//   hashtags.forEach((tag) => {
+//     // console.log(re.test(tag));
+//     // console.log(hashtags);
+//     // alertString += (re.test(tag)) ? '' : (reSpace.test(tag) ? 'Поставьте пробел между хештегами\n' : 'Вы ввели недопустимый хештег\n');
+//     if (re.test(tag) || tag === '') {
+//       // console.log(re.test(tag));
+//       alertString = '';
+//     }
+//     // if (reSpace.test(tag)) {
+//     //   alertString += 'Поставьте пробел между хештегами\n';
+//     // }
+//     alertString = 'Вы ввели недопустимый хештег\n';
+//   });
+
+//   if (hashtags.length > HASHTAG_COUNT) {
+//     alertString += 'Вы превысили допустимое количество хештегов\n';
+//   }
+
+//   hashtagField.setCustomValidity(alertString);
+//   hashtagField.reportValidity();
+// };
+
+
 const validateHashtag = (value) => {
 
   let alertString = '';
-  const re = /^#[A-Za-zА-Яа-я0-9]{1,19}$/gi;
-  const reSpace = /\b#\b/;
-  // re.test('#хэштег'); // > true
-  const hashtags = value.trim().toLowerCase().split([' ']);
+  const reHashtag = /^#[A-Za-zА-Яа-я0-9]{1,19}$/i;
+  const reDoubleHash = /\b#\b/;
+  const reSpaceEnd = /\s\s/;
+  const reSpaceReplace = /  +/g;
+  const oneSpace = ' ';
 
-  if (!value) {
-    alertString = '';
-  }
+  if (value) {
 
-  hashtags.forEach((tag) => {
-    console.log(re.test(tag));
-    console.log(hashtags);
-    // alertString += (re.test(tag)) ? '' : (reSpace.test(tag) ? 'Поставьте пробел между хештегами\n' : 'Вы ввели недопустимый хештег\n');
-    if (re.test(tag)) {
-      // console.log(re.test(tag));
-      alertString = '';
+    if (reSpaceEnd.test(value)) {
+      value = value.replace(reSpaceReplace, oneSpace);
     }
-    // if (reSpace.test(tag)) {
-    //   alertString += 'Поставьте пробел между хештегами\n';
-    // }
-    else
-    {alertString = 'Вы ввели недопустимый хештег\n';}
-  });
+    console.log(`value=${  value}`);
+    const hashtags = value.trim().toLowerCase().split([' ']);
+    const checkTag = (tag) => reHashtag.test(tag);
+    const matchTag = (tag) => tag.match(reDoubleHash);
 
-  if (hashtags.length > HASHTAG_COUNT) {
-    alertString += 'Вы превысили допустимое количество хештегов\n';
+    if (!hashtags.every(checkTag)) {
+      alertString += 'Вы ввели недопустимый хештег\n';
+      if (hashtags.some(matchTag)) {
+        alertString = 'Поставьте пробел между хештегами\n';
+      }
+    }
+
+    if (new Set(hashtags).size !== hashtags.length) {
+      const duplicate = hashtags.filter(((a) => (b) => a.has(b) || !a.add(b))(new Set));
+      alertString += `Удалите дублирующийся тег ${  duplicate}`;
+      console.log(duplicate);
+    }
+
+    if (hashtags.length > HASHTAG_COUNT) {
+      alertString += 'Вы превысили допустимое количество хештегов\n';
+    }
   }
-
   hashtagField.setCustomValidity(alertString);
   hashtagField.reportValidity();
 };
-
 
 hashtagField.addEventListener('input', debounce(() => {
   validateHashtag(hashtagField.value);
